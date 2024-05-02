318da9a566fe56ed1d531197169382442c2ac8bb
Elias M Olivares <elias.manque.o@gmail.com>
2024-05-02 18:26:42 -0400

    Update

---
+++ src/pages/ForgotPass.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function ForgotPass(){
+
+
+    return(
+        <></>
+    )
+}
+
+export default ForgotPass;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import ForgotPass from './ForgotPass';
+import { Link } from 'react-router-dom';
+
+function Login(){
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', pass);
+        // Aquí puedes añadir la lógica para autenticar al usuario
+      };
+
+
+    return (
+        <div>
+          <h2>Iniciar Sesión</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Contraseña:</label>
+              <input
+                type="password"
+                id="password"
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+                required
+            />
+            </div>
+
+            <div>
+                <p>Olvide la contraseña.<Link to={"./ForgotPass"}> Recuperar</Link> </p>
+            </div>
+
+
+            <button type="submit">Iniciar Sesión</button>
+            </form>
+        </div>
+        );
+}
+
+export default Login;
