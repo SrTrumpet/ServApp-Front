b94d3aea08560124d1f09267117d6598a59304d3
Crissss17 <cristian.moyano@alumnos.ucn.cl>
2024-07-02 02:23:20 -0400

    Update MisServicios.tsx

--- src/pages/MisServicios.tsx
+++ src/pages/MisServicios.tsx
@@ -7,7 +7,7 @@ function MisServicios(){
     return (
         <div className="bg-[#95D5B2] h-screen">
             <NavBar/>
-            <h1 className="text-5xl font-bold text-[#1B4332] text-center mb-10 ">Estas en tus servicios</h1>
+            <h1 className="text-5xl font-bold text-[#1B4332] text-center">Tus servicios</h1>
             <UserFindUser/>
             </div>
     );
